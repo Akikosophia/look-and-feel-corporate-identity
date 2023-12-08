9fc03d3fcd1ee7faf0c84b274561d17a710a8686
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2023-12-08 10:00:20 +0100

    update 6

---
+++ script.js
@@ -0,0 +1,10 @@
+function myfun(){
+    document.getElementById
+     ('form-wrapper').classList.add
+      ('active');
+   }
+  
+  function closer(){
+     document.getElementById('form-wrapper')
+    .classList.remove('active');
+  }
