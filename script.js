7135e65708278a9b9e5c183b452bd41b60ce2726
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-04-23 14:40:34 +0200

    -

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-function myfun(){
-    document.getElementById
-     ('form-wrapper').classList.add
-      ('active');
-   }
+// function myfun(){
+//     document.getElementById
+//      ('form-wrapper').classList.add
+//       ('active');
+//    }
   
-  function closer(){
-     document.getElementById('form-wrapper')
-    .classList.remove('active');
-  }
+//   function closer(){
+//      document.getElementById('form-wrapper')
+//     .classList.remove('active');
+//   }
+
+// const dialog = document.querySelector("dialog");
+// const showButton = document.querySelector("dialog + button");
+// const closeButton = document.querySelector("dialog button");
+
+// // "Show the dialog" button opens the dialog modally
+// showButton.addEventListener("click", () => {
+//   dialog.showModal();
+// });
+
+// // "Close" button closes the dialog
+// closeButton.addEventListener("click", () => {
+//   dialog.close();
+// });
+
+const showDialogBtn = document.getElementById('showDialogBtn');
+const favDialog = document.getElementById('favDialog');
+
+showDialogBtn.addEventListener('click', () => favDialog.showModal());
